906949b416e5fcb09f939b6c9d4254f035b71a57
nwiestda <nwiestda@5c99aa24-93f7-4b87-a60b-f63d66337ca9>
2010-12-08 09:43:20 +0000

    Fixing weird symlink in svn:  part 2
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/shanoir/qtshanoir@899 5c99aa24-93f7-4b87-a60b-f63d66337ca9

---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <QApplication>
+#include <iostream>
+#include <QFileDialog>
+
+
+//#include <Arguments.hh>
+#include "qt_shanoir.h"
+
+
+#include <QTextStream>
+
+#ifdef WIN32
+void myMessageOutput(QtMsgType type, const char *msg)
+{
+    switch (type) {
+    case QtDebugMsg:
+        fprintf(stdout, "Debug: %s\n", msg);
+        break;
+    case QtWarningMsg:
+        fprintf(stdout, "Warning: %s\n", msg);
+        break;
+    case QtCriticalMsg:
+        fprintf(stdout, "Critical: %s\n", msg);
+        break;
+    case QtFatalMsg:
+        fprintf(stdout, "Fatal: %s\n", msg);
+        abort();
+    }
+}
+
+#endif
+
+int main(int ac,  char* av[])
+{
+#ifdef WIN32
+    qInstallMsgHandler(myMessageOutput);
+#endif
+    // DJDecoderRegistration::registerCodecs();
+    QCoreApplication::setOrganizationName("Neurinfo/CHU-Rennes");
+    QCoreApplication::setOrganizationDomain("neurinfo.org");
+    QCoreApplication::setApplicationName("MultiMRView");
+
+
+    QApplication*  app = new QApplication(ac, av);
+
+    {
+        QFile fsheet(":/StyleSheets/MRView.css");
+        fsheet.open(QFile::ReadOnly);
+        QTextStream in(&fsheet);
+        QString sheet = in.readAll();
+        qApp->setStyleSheet(sheet);
+    }
+    /*
+    QScriptEngine engine;
+
+#if !defined(QT_NO_SCRIPTTOOLS)
+    QScriptEngineDebugger debugger;
+    debugger.attachTo(&engine);
+    QMainWindow *debugWindow = debugger.standardWindow();
+    debugWindow->resize(1024, 640);
+    debugWindow->show();
+
+#endif
+    */
+
+    std::cout << "Started\n";
+    QtShanoir win;
+    //  win.main(ac, av);
+
+    //    engine.newQObject(&win, QScriptEngine::AutoOwnership, QScriptEngine::PreferExistingWrapperObject);
+
+
+    win.show();
+    app->exec();
+
+    // DJDecoderRegistration::cleanup();
+    return 0;
+}
