fc06cdfdf4199d51f0d38fd2671c85798b79e9a2
Alexandre Abadie <Alexandre.Abadie@irisa.fr>
2011-10-20 11:35:43 +0200

    add license header in the code, add README, LICENSE, AUTHORS and COPYING files

--- Code/QtShanoir.cpp
+++ Code/QtShanoir.cpp
@@ -1,9 +1,21 @@
 /*
- * QtShanoir.cpp
- *
- *  Created on: 6 janv. 2011
- *      Author: aabadie
- */
+    The QtShanoir library.
+    Copyright (C) 2011  INRIA, Université de Rennes 1
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 #include <QtShanoir.h>
 #include <QtShanoirSettings.h>
