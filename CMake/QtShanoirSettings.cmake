f976fc7313914c03036003b14c82d9aabf6e5fec
Olivier Commowick <olivier.commowick@inria.fr>
2019-11-04 14:29:38 +0100

    Update for windows compilation

--- CMake/QtShanoirSettings.cmake
+++ CMake/QtShanoirSettings.cmake
@@ -10,11 +10,16 @@ SET (INSTALL_MAN /man/)
 SET (INSTALL_SHARE /share/)
 
 ## SETTING THE INCLUDE DIRS
+
 SET (QTSHANOIR_INCLUDE_DIRS
 ## Modules include dirs
   ${QTSHANOIR_SOURCE_DIR}/Code
   ${QTSHANOIR_BINARY_DIR}
   ${QTSHANOIR_BINARY_DIR}/qtshanoir_autogen/include
+  ${QTSHANOIR_BINARY_DIR}/qtshanoir_autogen/include_Debug
+  ${QTSHANOIR_BINARY_DIR}/qtshanoir_autogen/include_Release
+  ${QTSHANOIR_BINARY_DIR}/qtshanoir_autogen/include_MinSizeRel
+  ${QTSHANOIR_BINARY_DIR}/qtshanoir_autogen/include_RelWithDebInfo
   ${ZLIB_INCLUDE_DIRS}
 )
 
